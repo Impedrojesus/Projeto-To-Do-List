ca599ac7c2e2221e3ad68f4c84625c0a6af1d106
Pedro Henrique de Jesus Santos <114628005+Impedrojesus@users.noreply.github.com>
2024-03-08 23:57:08 -0300

    Add files via upload

---
+++ Projeto To-Do-List/todolist/src/main/java/com/project/todolist/task/ITaskRepository.java
@@ -0,0 +1,10 @@
+package com.project.todolist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ITaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByIdUser(UUID idUser);
+}
---
+++ Projeto To-Do-List/todolist/src/main/java/com/project/todolist/user/IUserRepository.java
@@ -0,0 +1,8 @@
+package com.project.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<User,  UUID> {
+    User findByUsername(String username);
+}
